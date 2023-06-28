eb9fa8a609c79b7aa29079ae1f350145c9c6b701
William <williamjuan_16santos@hotmail.com>
2023-06-28 15:46:06 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode () {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+
+
+    // substituir a imagem
+    if(html.classList.contains("light")) {
+        // se tiver light mode, adicionar a imagem light
+        img.setAttribute("src", "./Assets/avatar-light.png")
+    } else {
+        // se tiver sem ligth mode, manter a imagem normal
+        img.setAttribute("src", "./Assets/avatar.png")
+    } 
+
+
+
+}
+
